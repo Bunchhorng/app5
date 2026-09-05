6400da2b566e5da29c80e916b1d4089a042bf897
Roeung Bunchhorng <roeungbunchhorng37@gmail.com>
2026-09-05 13:36:20 +0700

    update helloWord

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
     <h1>Hello Kon kon, Is it easy?</h1>
+    <h2>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime, laudantium itaque? Voluptate quidem nostrum accusantium voluptatibus molestiae sed, maxime necessitatibus.</h2>
   </div>
 </template>
